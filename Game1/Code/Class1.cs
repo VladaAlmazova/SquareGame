fab7d24fdb7673c8476e00912d476fad15841443
VladaAlmazova <almazova.2004@mail.ru>
2023-04-11 23:13:01 +0500

    первое изменение

---
+++ Game1/Code/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1
+{
+    class Entity
+    {
+        public static int Width, Height;
+        public static SpriteBatch SpriteBatch { get; set; }
+        static public Character Character { get; set; }
+
+        public static void InIt(SpriteBatch spriteBatch, int width, int height)
+        {
+            SpriteBatch = spriteBatch;
+            Width = width;
+            Height = height;
+            Character = new Character(new Vector2(0, 0));
+        }
+        
+        static public void Draw()
+        {
+            Character.Draw();
+        }
+    }
+
+
+    class Character: Entity
+    {
+        Color color = Color.Black;
+        public static Texture2D texture2D { get; set; }
+        Vector2 Pos;
+        public Character(Vector2 pos)
+        {
+            Pos = pos;
+        }
+
+        public void Draw()
+        {
+            Entity.SpriteBatch.Draw(texture2D, Pos = new Vector2(0, Height-200), color);
+        }
+    }
+}
--- Game1/Game1.cs
+++ Game1/Game1.cs
@@ -1,17 +1,18 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
 
 namespace Game1
 {
     public class Game1 : Game
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
 
         public Game1()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -19,19 +20,28 @@ namespace Game1
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            graphics.PreferredBackBufferWidth = 1680;
+            graphics.PreferredBackBufferHeight = 900;
+            graphics.IsFullScreen = false;
+            graphics.ApplyChanges();
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            Entity.InIt(spriteBatch, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight);
+            Character.texture2D = Content.Load<Texture2D>("character");
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
+            var K = Keyboard.GetState();
+            if(K.IsKeyDown(Keys.Right))
+            {
+
+            }
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
@@ -42,7 +52,11 @@ namespace Game1
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.WhiteSmoke);
+            spriteBatch.Begin();
+            Entity.Draw();
+            spriteBatch.End();
+
 
             // TODO: Add your drawing code here
 
