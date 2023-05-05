92abdc2776bc277e5f458f1d54d32aba51a031e2
VladaAlmazova <almazova.2004@mail.ru>
2023-05-06 03:28:56 +0500

    добавление ещё двух платформ

--- Game1/Code/Class1.cs
+++ Game1/Code/Class1.cs
@@ -11,91 +11,107 @@ namespace Game1
 {
    class Entity
     {
-        private Vector2 pos_char = new Vector2(50, 900 - 113 - 50);
-        private const int size = 122;
-        const int indent = 10;
+        private const int size = 120;
+        const int indent = 0;//10; кратное 5
 
         public static int Width, Height;
         public static SpriteBatch SpriteBatch { get; set; }
         static public Character Character { get; set; }
-
-        static public Platform Platform1 { get; set; }
 
         public static void InIt(SpriteBatch spriteBatch, int width, int height)
         {
             SpriteBatch = spriteBatch;
             Width = width;
             Height = height;
-            Platform1 = new Platform(new Vector2(578, 550), 10);
-            Character = new Character(new Vector2(indent, Height - size- indent), Platform1);
-            //Character = new Character(Vector2.Zero, Platform1);
+            Character = new Character(new Vector2(200, 800));
         }
 
 
         static public void Draw(GameTime gameTime)
         {
             Character.Draw(gameTime);
-            Platform1.Draw(gameTime);
+            for(int i = 0; i < PackPlatforms.platforms.Count; i++)
+                PackPlatforms.platforms[i].Draw(gameTime);
         }
 
         public static bool IsInMap(Vector2 pos)
         {
-            return !(pos.X < indent || pos.X > Width - size || pos.Y < indent || pos.Y > Height - size - indent);    
+            return !(pos.X < indent || pos.X + size > Width - indent || pos.Y < indent || pos.Y + size > Height - indent);    
         }
     }
 
+    class PackPlatforms: Entity
+    {
+        static public List<Platform> platforms = new List<Platform>()
+        {
+            new Platform(new Vector2(400, 700), 10),
+            new Platform(new Vector2(600, 600), 10),
+            new Platform(new Vector2(800, 460), 10),
+        };
+    }
 
     class Character: Entity
     {
         Color color = Color.White;
         public static Texture2D texture2D { get; set; }
         public Vector2 Pos;
-        private Platform Platform = new Platform(Vector2.Zero, 0);
-        public List<Platform> listPlat = new List<Platform>()
-        {
-            new Platform(new Vector2(800, 550), 15),
-            new Platform(new Vector2(800, 300), 11),
-            new Platform(new Vector2(700, 250), 10),
-        };
-        public const int size = 122;
+        public const int size = 120;
         public int speed = 10;
-        public Character(Vector2 pos, Platform platform)
+        public int gravity = 10;
+        public Character(Vector2 pos)
         {
             Pos = pos;
-            Platform = platform;
         }
-
-        private bool VectorsIntersect(Tuple<Vector2, Vector2> vert, Tuple<Vector2, Vector2>gor)
-        {
-            return gor.Item1.X < vert.Item1.X && vert.Item1.X < gor.Item2.X &&
-                vert.Item1.Y < gor.Item1.Y && gor.Item1.Y < vert.Item2.Y;
-        }
-
 
         public Keys lastKey = Keys.S;
         private bool Fall = false;
 
-        private void CanFall(Vector2 pos)
+        private bool CorrectPositionWithPlat(Vector2 pos, Platform platform)
+        {
+            return IsInMap(pos) && !platform.IsInPlatform(pos);
+        }
+
+        private bool CorrectPositionWithAllPlat(Vector2 pos)
+        {
+            for (int i = 0; i < PackPlatforms.platforms.Count; i++)
+                if (!CorrectPositionWithPlat(pos, PackPlatforms.platforms[i]))
+                    return false;
+            return true;
+        }
+
+        private void CanFall(Vector2 pos) //3plat
         {
             pos.Y += 5;
-            if (IsInMap(pos) && !Platform.IsInPlatform(pos))
+            if(CorrectPositionWithAllPlat(pos))
                 Fall = true;
             pos.Y -= 5;
-        }
-        /*private void CanFall(Vector2 pos)
-        {
-            pos.Y += 5;
-            foreach(var plat in listPlat)
-            {
-                if (IsInMap(pos) && !plat.IsInPlatform(pos))
-                {
-                    Fall = true;
-                    break;
-                }  
-            }            
-            pos.Y -= 5;
-        }*/
 
+            if (lastKey == Keys.Up)
+                Fall = true;
+        }
+
+        private Vector2 PressingButton(Vector2 posTest, Keys key, int speed)
+        {
+            switch (key)
+            {
+                case Keys.Left:
+                    posTest.X -= speed;
+                    CanFall(posTest);
+                    break;
+                case Keys.Right:
+                    posTest.X += speed;
+                    CanFall(posTest);
+                    break;
+                case Keys.Up:
+                    posTest.Y -= speed;
+                    break;
+                default:
+                    key = lastKey;
+                    break;
+            }
+            lastKey = key;
+            return posTest;
+        }
 
         public void Update(Keys[] keys)
         {
@@ -104,65 +120,22 @@ namespace Game1
             {
                 if (keys.Length > 0)
                 {
-                    switch (keys[0])
-                    {
-                        case Keys.Left:
-                            if (lastKey == Keys.Up)
-                                Fall = true;
-                            posTest.X -= speed;
-                            lastKey = Keys.Left;
-                            CanFall(posTest);
-                            break;
-                        case Keys.Right:
-
-                            if (lastKey == Keys.Up)
-                                Fall = true;
-                            posTest.X += speed;
-                            lastKey = Keys.Right;
-                            CanFall(posTest);
-                            break;
-                        case Keys.Up:
-                            posTest.Y -= speed;
-                            lastKey = Keys.Up;
-                            break;
-                        default:
-                            break;
-                    }
+                    posTest = PressingButton(posTest, keys[0], speed);
                 }
-            }
-            var speed_e = speed;
+            }            
             if (Pos == posTest)
             {
+                var speed_e = speed;
                 if (Fall)
                     speed_e = speed / 2;
-                /*else
-                    speed_e = speed;*/
-                switch (lastKey)
-                {
-                    case Keys.Left:
-                        posTest.X -= speed_e;
-                        CanFall(posTest);
-                        break;
-                    case Keys.Right:
-                        posTest.X += speed_e;
-                        CanFall(posTest);
-                        break;
-                    case Keys.Up:
-                        posTest.Y -= speed_e;
-                        break;
-                    /*case Keys.Down:
-                        posTest.Y += 20;
-                        break;*/
-                    default:
-                        break;
-                }
-                
-                    
+                posTest = PressingButton(posTest, lastKey, speed_e);                    
             }
-            if (IsInMap(posTest) && !Platform.IsInPlatform(posTest))
+
+            if (CorrectPositionWithAllPlat(posTest))       //3 plat
             {
                 Pos = posTest;
             }
+
             else
             {
                 Fall = true;
@@ -171,22 +144,22 @@ namespace Game1
                     lastKey = Keys.S;
             }
             if (Fall)    
-                posTest.Y += 10;
-            if (IsInMap(posTest) && !Platform.IsInPlatform(posTest))
+                posTest.Y += gravity;
+
+            if (CorrectPositionWithAllPlat(posTest))       //3 plat
             {
                 Pos = posTest;
             }
+
             else
             {
                 Fall = false;
-                //lastKey = Keys.S;
             }
                 
         }
 
         public void Draw(GameTime gameTime)
         {
-            //Entity.SpriteBatch.Draw(texture2D, Pos = new Vector2(0, Height-200), color);
             Entity.SpriteBatch.Draw(texture2D, Pos, color);
         }
     }
@@ -231,9 +204,9 @@ namespace Game1
         {
             var ostPos = Pos;
             for(var i = 0; i < countPlat; i++)
-            {
-                ostPos.X += Height;
+            {                
                 Entity.SpriteBatch.Draw(texture2D, ostPos, color);
+                ostPos.X += Height;
             }
         }
 
@@ -243,15 +216,15 @@ namespace Game1
 
         public bool IsInPlatform(Vector2 ChPos)
         {          
-            var ClUp = new Vector2(ChPos.X, ChPos.Y);
-            var CrDow = new Vector2(ChPos.X + Character.size, ChPos.Y + Character.size);
-            if ((CrDow.X > l_up_p.X && CrDow.X < r_down_p.X) || (ClUp.X > l_up_p.X && ClUp.X < r_down_p.X))
+            var l_ClUp = new Vector2(ChPos.X, ChPos.Y);
+            var r_CrDow = new Vector2(ChPos.X + Character.size, ChPos.Y + Character.size);
+            if ((l_up_p.X < r_CrDow.X && r_CrDow.X <= r_down_p.X) || (l_up_p.X <= l_ClUp.X && l_ClUp.X < r_down_p.X))
             {
-                if (CrDow.Y > l_up_p.Y && CrDow.Y < r_down_p.Y)
+                if (l_up_p.Y < r_CrDow.Y && r_CrDow.Y <= r_down_p.Y)
                     return true;
-                if (ClUp.Y < r_down_p.Y && ClUp.Y > l_up_p.Y)
+                if (l_up_p.Y <= l_ClUp.Y && l_ClUp.Y < r_down_p.Y)
                     return true;
-                if (ClUp.Y < l_up_p.Y && CrDow.Y > r_down_p.Y)
+                if (l_ClUp.Y <= l_up_p.Y && r_down_p.Y <= r_CrDow.Y)
                     return true;
             }
             return false;
--- Game1/Game1.cs
+++ Game1/Game1.cs
@@ -11,9 +11,6 @@ namespace Game1
         private SpriteBatch spriteBatch;
         Texture2D texture;
 
-        //private Vector2 pos_char = new Vector2(50, 900 - 113 - 50);
-        //private const int size = 113;
-
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -23,9 +20,8 @@ namespace Game1
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
             graphics.PreferredBackBufferWidth = 1680;
-            graphics.PreferredBackBufferHeight = 900;
+            graphics.PreferredBackBufferHeight = 920;//900;
             graphics.IsFullScreen = false;
             graphics.ApplyChanges();
             base.Initialize();
@@ -35,9 +31,8 @@ namespace Game1
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
             Entity.InIt(spriteBatch, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight);
-            Character.texture2D = Content.Load<Texture2D>("main_character"); //113*113
+            Character.texture2D = Content.Load<Texture2D>("character");//"main_character"); //113*113
             Platform.texture2D = Content.Load<Texture2D>("platform");//20*20
-           // texture = Content.Load<Texture2D>("platform");//20*20
         }
 
         protected override void Update(GameTime gameTime)
@@ -49,11 +44,7 @@ namespace Game1
                     Exit();
             }
             Entity.Character.Update(keys);
-            Entity.Platform1.Update(keys);
-
-            //pos_char = Entity.Character.Pos;
-
-            // TODO: Add your update logic here
+            //Entity.Platform1.Update(keys);
 
             base.Update(gameTime);
         }
@@ -62,16 +53,9 @@ namespace Game1
         {
             GraphicsDevice.Clear(Color.WhiteSmoke);
             spriteBatch.Begin();
-            /*for(var i = 0; i < 10; i++)
-                spriteBatch.Draw(texture, new Vector2(578+(20*i), 550), Color.White);*/
             Entity.Draw(gameTime);
 
             spriteBatch.End();
-
-            /*spriteBatch.Begin();
-            spriteBatch.Draw(texture, pos_char, Color.White);
-            spriteBatch.End();*/
-            // TODO: Add your drawing code here
 
             base.Draw(gameTime);
         }
